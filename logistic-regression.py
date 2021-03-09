6ef673c34c1ff352632ef2f91ea0ab60de554a32
Harvey Nighy <harveynighy@talktalk.net>
2021-03-09 01:04:43 +0000

    Have prepared the data
    Have prepared the data and am now going to design the model

--- logistic-regression.py
+++ logistic-regression.py
@@ -4,3 +4,36 @@
 #   -forward pass: compute prediction and loss
 #   -backward pass: gradients
 #   -update weights
+import torch
+import torch.nn
+import numpy as np 
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+#data
+bc = datasets.load_breast_cancer()
+X, y = bc.data, bc.target
+
+n_samples, n_features = X.shape
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+#scale
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+X_train = torch.from_numpy(X_train.astype(np.float32))
+X_test = torch.from_numpy(X_test.astype(np.float32))
+y_train = torch.from_numpy(y_train.astype(np.float32))
+y_test = torch.from_numpy(y_test.astype(np.float32))
+
+y_train = y_train.view(y_train.shape([0], 1))
+y_test = y_test.view(y_train.shape([0], 1))
+
+# Model
+
+
+
+# loss and optimizer
+
+# training loop
