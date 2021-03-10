83e90de42deff2c31587b3f66b07f3bc2f2aa1cc
Harvey Nighy <harveynighy@talktalk.net>
2021-03-10 01:15:46 +0000

    Have input parameters and data
    Added hyper parameters and sorted the data

---
+++ feedforward.py
@@ -0,0 +1,41 @@
+# MNIST
+# DataLoader, Transformation
+# Multilayer Neural Net, activation function
+# Loss and optimizer
+# Training Loop (batch training)
+# Model evaluation
+# GPU Support
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+#device config
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+#hyper parameters
+input_size = 784
+hidden_size = 100
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+#MNIST data
+training_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset=training_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+examples = iter(train_loader)
+samples, labels = examples.next()
+
+print(samples.shape, labels.shape)
+
+for i in range(6):
+    plt.subplot(2, 3, i+1)
+    plt.imshow(samples[i][0], cmap='gray')
+plt.show()
